639153cf92d6307b0221b09a04de6805374c78e6
T6509-0 <j30delossantos@gmail.com>
2024-11-11 07:48:09 +0800

    Update bible.js

--- script/bible.js
+++ script/bible.js
@@ -15,7 +15,7 @@ module.exports.run = async function ({ api, event }) {
 
     try {
         // Fetch a random Bible verse from the API
-        const response = await axios.get('https://joshweb.click/bible');
+        const response = await axios.get('https://jonellprojectccapisexplorer.onrender.com/api/randomverse');
 
         if (response.data && response.data[0]) {
             const verse = response.data[0];
