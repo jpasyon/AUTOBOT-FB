8ce003673b547f5ed0b81f4a1f0c5e353c0751f6
T6509-0 <j30delossantos@gmail.com>
2024-11-20 20:00:39 +0800

    Update offbot.js

--- script/offbot.js
+++ script/offbot.js
@@ -10,6 +10,6 @@ module.exports.config = {
 
 module.exports.run = ({ event, api }) => {
     const permission = global.config.GOD;
-    if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command", event.threadID, event.messageID);
-    api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot is now turned off.`, event.threadID, () => process.exit(0));
+    if (!permission.includes(event.senderID)) return api.sendMessage("You don't have permission to use this command.", event.threadID, event.messageID);
+    api.sendMessage("Bot is now turned off.", event.threadID, () => process.exit(0));
 };
