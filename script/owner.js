52a43666b3746751637f2bebca00d34c7875f406
jpasyon <dolorescastanedaj@gmail.com>
2024-11-26 00:27:01 +0800

    Update owner.js

--- script/owner.js
+++ script/owner.js
@@ -1,3 +1,4 @@
+
 const { OnChat } = require('chatbox-utility');
 
 module.exports["config"] = {
@@ -15,14 +16,14 @@ module.exports["run"] = async ({ api, event }) => {
     if (!event.isGroup) return chat.reply("Response:");
 
     const response = "Response:\n" +
-        "BOT NAME: Juno Delos Santos\n" +
-        "BOT OWNER: Juno Delos Santos\n" +
-        "HOBBY: Coding, Programming\n" +
+        "BOT NAME: Unknown\n" +
+        "BOT OWNER: Jesus Dolores Castañeda.\n" +
+        "HOBBY: Sleeping, Cooking\n" +
         "STATUS: Single\n" +
-        "AGE: 15\n" +
-        "SPORTS: Badminton\n" +
+        "AGE: 21\n" +
+        "SPORTS: Basketball\n" +
         "LOCATION: Philippines\n" +
-        "OWNERS LINK: https://www.facebook.com/profile.php?id=100091133423695\n";
+        "OWNERS LINK: https://www.facebook.com/profile.php?id=100093705110825\n";
 
     chat.reply(response);
 };
